2d3d9cab1f7ed7c02ede64170ecc164aa04e50b4
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2023-02-28 11:05:43 +0000

    intro to PHP

---
+++ LAB6/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $my_age = 16
+    ?>
+    <h2>
+    <?php
+    if ($my_age == 16){
+        print "You can buy specs";
+    } else if($my_age == 18) {
+        print "You can buy mugs";
+    } else if ($my_age == 21) {
+        print "You can buy sausage rolls";
+    }
+    ?>
+    </h2>
+    <p>
+        <?php
+            echo "Hello World!";
+            echo "Hello,".""."world"."!";
+            echo 5*7;
+            $myname = "Frodo Baggins";
+            $myage = 111;
+            echo "My Name is". $myname."and I am".$myage;
+            $name = "Edgar";
+            if($name == "Simon"){
+                print "I Know you!";
+            }
+            else {
+                print"Who are you?";
+            }
+        ?>
+    </p>
+    <p>
+        <?php
+            $numberOfHobbits = 2;
+            switch ($numberOfHobbits) {
+                case 1:
+                    echo "1 sad hobbit";
+                break;
+                case 2:
+                    echo "2 happy hobbits";
+                break;
+                case 3:
+                    echo "3 hobbits are a crowd";
+                break;
+                default:
+                echo "All the hobbits have gone home";
+            }
+        ?>
+    </p>
+    <?php
+        $myArray = array("do", "re", "mi");
+        echo $myArray[0];
+        $myArray[1] = "la";
+        echo $myArray[1];
+        unset($array[2]);
+    ?>
+</body>
+</html>
